589956c0d33f044ec7a4f083d69552ae682c0d0f
andreyvpng <andrewvarfolomeev@protonmail.com>
2018-08-03 00:47:10 +1000

    added models, views, urls for shop

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return '{}(Category)'.format(name)
+
+
+class Product(models.Model):
+    name= models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, \
+                                 null=True)
+    image = models.ImageField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
